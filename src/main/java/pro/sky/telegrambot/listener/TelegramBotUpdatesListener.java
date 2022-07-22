9912bed90f5f874581d4517c0617e3e0a9594c85
AnnaLihanova <anja.lihanova@gmail.com>
2022-07-22 11:53:23 +0300

    Добавила проверку даты

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -15,6 +15,7 @@ import pro.sky.telegrambot.repository.NotificationTaskRepository;
 import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,6 +26,9 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
     private static final String START_COMMAND = "/start";
     private static final String HELLO_TEXT = "Hello!";
+    private static final DateTimeFormatter DATA_TIME= DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+    private static final String REGEX_BOT_MESSAGE = "([\\d\\.\\:\\s]{16})(\\s)([\\W+]+)";
+    private static final LocalDateTime CURRENT_DATE = LocalDateTime.now();
 
     private final TelegramBot telegramBot;
     private final NotificationTaskRepository notificationTaskRepository;
@@ -44,28 +48,40 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+
+            // Текст сообщения из чата
+            String text = update.message().text();
+
+            //ID чата
+            Long chatId = update.message().chat().id();
+
             //Получаем текст сообщения из чата и отправляем ответ
             Message message = update.message();
             if (message.text().equals(START_COMMAND)) {
                 logger.info(START_COMMAND + "received");
                 sendMessage(message.chat().id(), HELLO_TEXT);
+                return;
             }
 
-            // Разбиваем пришедшее сообщение на дату и текст
-            String text = update.message().text();
-            Long chatId = update.message().chat().id();
-            Matcher matcher = Pattern.compile("([\\d\\.\\:\\s]{16})(\\s)([\\W+]+)").matcher(text);
+            Matcher matcher = Pattern.compile(REGEX_BOT_MESSAGE).matcher(text);
             if (!matcher.matches()) {
                 sendMessage(chatId, "Введено некорректное сообщение!");
                 return;
             }
+
+            // Дата и время уведомления
             String notificationDataTimeStr = matcher.group(1);
+
+            //Текст уведомления
             String notificationText = matcher.group(3);
 
             //Парсинг строки в LocalDateTime
             LocalDateTime notificationDataTime = LocalDateTime
-                    .parse(notificationDataTimeStr, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+                    .parse(notificationDataTimeStr, DATA_TIME);
+            if (notificationDataTime.isBefore(CURRENT_DATE)) {
+                sendMessage(chatId, "Дата указана неверно!");
+                return;
+            }
 
             //Создаем уведомление
             NotificationTask notificationTask = new NotificationTask(chatId, notificationText, notificationDataTime);
@@ -75,7 +91,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
             //Отправляем уведомление
             sendMessage(chatId, "Напоминание! " + notificationTask.getMessage()
-                    + " Я отправлю Вам уведомление в " + notificationTask.getDateTime().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
+                    + ". Я отправлю Вам уведомление " + notificationTask.getDateTime().format(DATA_TIME));
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
